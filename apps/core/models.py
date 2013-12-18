0bf790a5f6166efcf2648e0594a371d85667ff28
Derek Stegelman <dstegelman@gmail.com>
2013-12-17 21:01:31 -0700

    Restructure

--- apps/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PublishModel(models.Model):
-    published_date = models.DateTimeField()
-    published = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
--- conf/project.py
+++
@@ -1,4 +0,0 @@
-#Project details
-PROJECT_ID = 'example.com'
-PROJECT_USER = 'user'
-PROJECT_HOSTS = ['example.com']
